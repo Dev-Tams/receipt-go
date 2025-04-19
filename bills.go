2abac3d7b6df98455b33fe13a661fc47cb34319c
Dev-Tams <tammyalemu@gmail.com>
2025-04-19 06:00:43 +0100

    Refactor recivers with pointers

--- bills.go
+++ bills.go
@@ -9,6 +9,7 @@ type bill struct {
 	name  string
 	items map[string]float64
 	tip   float64
+	tax  float64
 }
 
 func newBill(name string) bill {
@@ -25,20 +26,33 @@ func newBill(name string) bill {
 
 //creating a reciever func to format bill
 
-func (b bill) format() string {
+func (b *bill) format() string {
 
 	fb := "Bill Items: \n"
+	/**/
 	var total float64 = 0.0
-	var tax float64 = total + 0.15
+	var tax float64 =  0.10
 
 	//cycle round the struct
 	for k, v := range b.items {
 		fb += fmt.Sprintf("%-20v....$%v\n", k+":", v)
-		total += tax + v
+		total += tax + v 
 	}
 
 	fb += fmt.Sprintf("%-20v....$%0.2f\n", "tax:", tax)
-	fb += fmt.Sprintf("%-20v....$%0.2f", "total:", total)
+	fb += fmt.Sprintf("%-20v....$%0.2f\n", "tip:", b.tip)
+	fb += fmt.Sprintf("%-20v....$%0.2f", "total:", total+b.tip)
 
 	return fb
+}
+
+//add tip
+func (b *bill) addTip(tip float64){
+	b.tip = tip
+}
+
+//add item to the bill
+
+func (b *bill) addItem(name string, price float64){
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 
 	mybill := newBill("mario's bill")
+	mybill.addTip(2)
+	mybill.addItem("beans", 7)
 
 	fmt.Println(mybill.format())
 }
